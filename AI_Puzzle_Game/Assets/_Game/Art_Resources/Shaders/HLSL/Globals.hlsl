3a1df3ca5bdc2ef6439868fa70d2810ef288cfe9
ArnulfoVargasMejia <vargas.mejia.arnulfo04@gmail.com>
2025-04-20 21:27:46 -0600

    Globalized lightning

--- AI_Puzzle_Game/Assets/_Game/Art_Resources/Shaders/HLSL/Globals.hlsl
+++ AI_Puzzle_Game/Assets/_Game/Art_Resources/Shaders/HLSL/Globals.hlsl
@@ -6,6 +6,16 @@ float4 _Global_BaseColor;
 float4 _Global_Color;
 float4 _Global_FluorColor;
 
+float4 _Global_Light_MainColor;
+float4 _Global_Light_AmbientColor;
+float4 _Global_Light_LightColor;
+half _Global_Light_SpecularIntensity;
+float4 _Global_Light_RimLightShadowColor;
+float4 _Global_Light_RimLightLightsColor;
+half _Global_Light_FresnelPowerLights;
+half _Global_Light_FresnelPower;
+half _Global_Light_SpecularPower;
+
 #endif
 
 void GetRadius_half(out float radius) {
@@ -22,4 +32,26 @@ void GetColor_float(out float4 color) {
 
 void GetFluorColor_float(out float4 fluor) {
     fluor = _Global_FluorColor;
+}
+
+void GetLightVariables_float(
+    out float4 MainColor,
+    out float4 AmbientColor,
+    out float4 LightColor,
+    out half SpecularIntensity,
+    out float4 RimLightShadow,
+    out float4 RimLightLights,
+    out half FresnelPowerLights,
+    out half FresnelPower,
+    out half SpecularPower
+    ) {
+    MainColor = _Global_Light_MainColor;
+    AmbientColor = _Global_Light_AmbientColor;
+    LightColor = _Global_Light_LightColor;
+    SpecularIntensity = _Global_Light_SpecularIntensity;
+    RimLightShadow = _Global_Light_RimLightShadowColor;
+    RimLightLights = _Global_Light_RimLightLightsColor;
+    FresnelPowerLights = _Global_Light_FresnelPowerLights;
+    FresnelPower = _Global_Light_FresnelPower;
+    SpecularPower = _Global_Light_SpecularPower;
 }
---
+++ AI_Puzzle_Game/Assets/_Game/_Scripts/Config/BaseLightningVariables.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "BaseLightningVariables", menuName = "BaseLightningVariables", order = 0)]
+public class BaseLightningVariables : ScriptableObject {
+    [SerializeField] Color MainColor, AmbientColor, LightColor;
+    [SerializeField, Range(0,1)] float SpecularIntensity, SpecularPower;
+    [SerializeField] Color RimLightShadow, RimLightLights;
+    [SerializeField] float FresnelPowerLights, FresnelPower;
+
+    void OnValidate()
+    {
+        Shader.SetGlobalColor("_Global_Light_MainColor", MainColor);
+        Shader.SetGlobalColor("_Global_Light_AmbientColor", AmbientColor);
+        Shader.SetGlobalColor("_Global_Light_LightColor", LightColor);
+        Shader.SetGlobalColor("_Global_Light_RimLightShadowColor", RimLightShadow);
+        Shader.SetGlobalColor("_Global_Light_RimLightLightsColor", RimLightLights);
+        
+        Shader.SetGlobalFloat("_Global_Light_SpecularIntensity", SpecularIntensity);
+        Shader.SetGlobalFloat("_Global_Light_FresnelPowerLights", FresnelPowerLights);
+        Shader.SetGlobalFloat("_Global_Light_FresnelPower", FresnelPower);
+        Shader.SetGlobalFloat("_Global_Light_SpecularPower", SpecularPower);
+    }
+}
